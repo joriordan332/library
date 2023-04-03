79336d7c8eff5150196e4ee2818594bde053a570
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-04-03 15:37:41 +0100

    drawTable function added & array cleared
    drawTable function added and called in the addBookToLibrary function that creates a card for each book entered
    array myLibrary emptied at the end of the function so that full array isn't added each time add book is clicked

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function Book(title, author, pages, read) {
 addBookToLibrary.prototype = Object.create(Book.prototype);
 
 document.getElementById('addBook');
-addBook.addEventListener('click', addBookToLibrary);
+addBook.addEventListener('click', addBookToLibrary) ;
 
 function addBookToLibrary() {
   title = document.getElementById("title").value;
@@ -19,9 +19,55 @@ function addBookToLibrary() {
   pages = document.getElementById("pages").value;
   read = document.getElementById("pages_read").value;
   let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  console.log(newBook);
+  myLibrary.push(newBook); 
+
+  drawTable()
 }
+
+function drawTable() { 
+  
+
+  myLibrary.forEach((library) => {
+    console.log(library)
+    //secondCells.removeChild(cellText);  array duplicating boks
+    //tableHeaders.removeChild(headerText);
+    const tbl = document.createElement("table");
+    const tblHead = document.createElement("thead");
+    const tblBody = document.createElement("tbody");
+
+    
+      // creates a table row
+      const firstRow = document.createElement("tr");
+      const secondRow = document.createElement("tr");
+  
+      
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        const secondCells = document.createElement("td");
+        const tableHeaders = document.createElement("th");
+        const cellText = document.createTextNode(`${library.title} ${library.author} ${library.pages} ${library.read}`);
+        const headerText = document.createTextNode(`${'title'} ${'author'} ${'pages'} ${'read'}`);
+        firstRow.appendChild(tableHeaders);
+        tableHeaders.appendChild(headerText);
+        secondCells.appendChild(cellText);
+        secondRow.appendChild(secondCells);
+        
+      
+  
+      // add the row to the end of the table body
+      tblHead.appendChild(firstRow);
+      tblBody.appendChild(secondRow);
+
+    
+      tbl.appendChild(tblHead);
+      tbl.appendChild(tblBody);
+      document.body.appendChild(tbl);
+  });
+  myLibrary.length = 0;
+}
+
+
 
 
 
