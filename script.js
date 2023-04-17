c383636f93df50f1c92a338dc6d146af0b67b4d5
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-04-17 11:29:57 +0100

    If statements & prompt added
    If statements added to addBookToLibrary function and addBook eventListener that prompt the user with a message if add book is clicked and all of the book details aren't filled out and it won't add the book if they aren't
    If all of the details are filled out and add book is clicked the details entered are cleared and the text in the input boxes returns to the placeholder text

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const promptText = document.getElementById('prompt');
 const myLibrary = [{
-  title: "A Game of Thrones",
-  author: "George R. R. Martin",
+  title: 'A Game of Thrones',
+  author: 'George R. R. Martin',
   pages: 694,
-  read: "Yes",
+  read: 'Yes',
 }];
 
 function Book(title, author, pages, read) {
@@ -10,168 +11,134 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.id = `${title}_${Date.now()}`
-  // the constructor...
+  this.id = `${title}_${Date.now()}`;
+}
+
+function addBookToLibrary() {
+  if (title.value === '' || author.value === '' || pages.value === '') {
+    promptText.textContent = 'Please fill out all of the book details';
+    return false;
+  }
+  if (title.value !== '' && author.value !== '' && pages.value !== '') {
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value;
+    read = document.getElementById('book_read').value;
+
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+  }
+  drawTable();
 }
 
 addBookToLibrary.prototype = Object.create(Book.prototype);
 
 document.getElementById('addBook');
-addBook.addEventListener('click', addBookToLibrary) ;
+addBook.addEventListener('click', (addBookToLibrary));
+addBook.addEventListener('click', () => {
+  if (title.value !== '' && author.value !== '' && pages.value !== '') {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    promptText.textContent = '';
+  }
+});
 
-function addBookToLibrary() {
-  title = document.getElementById("title").value;
-  author = document.getElementById("author").value;
-  pages = document.getElementById("pages").value;
-  read = document.getElementById("pages_read").value;
-  
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook); 
-
-  drawTable()
-
-}
-
-function drawTable() { 
-  
-
+function drawTable() {
   myLibrary.forEach((library) => {
-    
-    console.log(library)
-    //secondCells.removeChild(cellText);  array duplicating boks
-    //tableHeaders.removeChild(headerText);
-    const tbl = document.createElement("table");
+    const tbl = document.createElement('table');
+    const tblHead = document.createElement('thead');
+    const tblBody = document.createElement('tbody');
 
+    const firstRow = document.createElement('tr');
+    const secondRow = document.createElement('tr');
 
-    const tblHead = document.createElement("thead");
+    const secondCells = document.createElement('td');
+    const secondCellsone = document.createElement('td');
+    const secondCellstwo = document.createElement('td');
+    const secondCellsthree = document.createElement('td');
+    const secondCellsfour = document.createElement('td');
+    const secondCellsfive = document.createElement('td');
 
-    const tblBody = document.createElement("tbody");
+    const tableHeaders = document.createElement('th');
+    const tableHeadersone = document.createElement('th');
+    const tableHeaderstwo = document.createElement('th');
+    const tableHeadersthree = document.createElement('th');
 
-    
-      // creates a table row
-      const firstRow = document.createElement("tr");
-      const secondRow = document.createElement("tr");
-  
-      
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        const secondCells = document.createElement("td");
-        const secondCellsone = document.createElement("td");
-        const secondCellstwo = document.createElement("td");
-        const secondCellsthree = document.createElement("td");
-        const secondCellsfour = document.createElement("td");
-        const secondCellsfive = document.createElement("td");
+    const cellText = document.createTextNode(library.title);
+    const authorText = document.createTextNode(library.author);
+    const pagesText = document.createTextNode(library.pages);
+    const textread = document.createTextNode(library.read);
 
+    const headerText = document.createTextNode('Title');
+    const headerTextone = document.createTextNode('Author');
+    const headerTexttwo = document.createTextNode('Pages');
+    const headerTextthree = document.createTextNode('Read');
 
-        const tableHeaders = document.createElement("th");
-        const tableHeadersone = document.createElement("th");
-        const tableHeaderstwo = document.createElement("th");
-        const tableHeadersthree = document.createElement("th");
+    firstRow.appendChild(tableHeaders);
+    firstRow.appendChild(tableHeadersone);
+    firstRow.appendChild(tableHeaderstwo);
+    firstRow.appendChild(tableHeadersthree);
 
+    tableHeaders.appendChild(headerText);
+    tableHeadersone.appendChild(headerTextone);
+    tableHeaderstwo.appendChild(headerTexttwo);
+    tableHeadersthree.appendChild(headerTextthree);
 
+    secondCells.appendChild(cellText);
+    secondCellsone.appendChild(authorText);
+    secondCellstwo.appendChild(pagesText);
+    secondCellsthree.appendChild(textread);
 
+    secondRow.appendChild(secondCells);
+    secondRow.appendChild(secondCellsone);
+    secondRow.appendChild(secondCellstwo);
+    secondRow.appendChild(secondCellsthree);
+    secondRow.appendChild(secondCellsfour);
+    secondRow.appendChild(secondCellsfive);
 
-        const cellText = document.createTextNode(library.title);
-        const authorText = document.createTextNode(library.author);
-        const pagesText = document.createTextNode(library.pages);
-        const textread = document.createTextNode(library.read);
+    const readButton = document.createElement('button');
+    const readText = document.createTextNode('Change read status');
+    const deleteButton = document.createElement('button');
+    const deleteText = document.createTextNode('Delete');
+    secondCellsfour.appendChild(readButton);
+    readButton.appendChild(readText);
+    secondCellsfive.appendChild(deleteButton);
+    deleteButton.appendChild(deleteText);
 
+    const idData = document.createElement('td');
+    idData.textContent = `${Date.now()}`;
 
+    tblHead.appendChild(firstRow);
+    tblBody.appendChild(secondRow);
 
+    tbl.appendChild(tblHead);
+    tbl.appendChild(tblBody);
+    document.body.appendChild(tbl);
 
+    deleteButton.addEventListener('click', (book) => {
+      if (book.id === Object.id) {
+        tbl.remove();
+      }
+    });
 
-
-        const headerText = document.createTextNode('Title');
-        const headerTextone = document.createTextNode('Author');
-        const headerTexttwo = document.createTextNode('Pages');
-        const headerTextthree = document.createTextNode('Read');
-
-        
-        firstRow.appendChild(tableHeaders);
-        firstRow.appendChild(tableHeadersone);
-        firstRow.appendChild(tableHeaderstwo);
-        firstRow.appendChild(tableHeadersthree);
-
-
-
-
-
-
-        tableHeaders.appendChild(headerText);
-        tableHeadersone.appendChild(headerTextone);
-        tableHeaderstwo.appendChild(headerTexttwo);
-        tableHeadersthree.appendChild(headerTextthree);
-
-
-
-
-        secondCells.appendChild(cellText);
-        secondCellsone.appendChild(authorText);
-        secondCellstwo.appendChild(pagesText);
-        secondCellsthree.appendChild(textread);
-
-
-        secondRow.appendChild(secondCells);
-        secondRow.appendChild(secondCellsone);
-        secondRow.appendChild(secondCellstwo);
-        secondRow.appendChild(secondCellsthree);
-        secondRow.appendChild(secondCellsfour);
-        secondRow.appendChild(secondCellsfive);
-        
-
-        const readButton = document.createElement("button")
-        const readText = document.createTextNode("Change read status");
-        const deleteButton = document.createElement("button")
-        const deleteText = document.createTextNode("Delete");
-        secondCellsfour.appendChild(readButton);
-        readButton.appendChild(readText);
-        secondCellsfive.appendChild(deleteButton);
-        deleteButton.appendChild(deleteText);
-        
-
-        let idData = document.createElement("td")
-        idData.textContent = `${Date.now()}`
-
-  
-      // add the row to the end of the table body
-      tblHead.appendChild(firstRow);
-      tblBody.appendChild(secondRow);
-
-    
-      tbl.appendChild(tblHead);
-      tbl.appendChild(tblBody);
-      document.body.appendChild(tbl);
-                                      
-      deleteButton.addEventListener("click", (book)=> {
-        if(book.id === Object.id){
-          tbl.remove()
-        }
-        });
-
-        readButton.addEventListener("click", () => {
-          if (textread.textContent == 'Yes') {
-            textread.textContent = 'No'
-          }
-          else if (textread.textContent == 'No') {
-            textread.textContent = 'Yes'
-          }
-        });
-        
+    readButton.addEventListener('click', () => {
+      if (textread.textContent === 'Yes') {
+        textread.textContent = 'No';
+      } else if (textread.textContent === 'No') {
+        textread.textContent = 'Yes';
+      }
+    });
   });
   myLibrary.length = 0;
 }
 
-
-
-
-
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  document.getElementById('myForm').style.display = 'block';
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  document.getElementById('myForm').style.display = 'none';
 }
 
-drawTable()
+drawTable();
